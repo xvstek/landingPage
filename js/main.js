94e94f42becdaee2049b17579667632be794498d
xvstek <164932017+xvstek@users.noreply.github.com>
2024-03-25 19:30:15 -0500

    trying video.play() method for mobile

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@
 
 window.onload = function () {
 
+    console.log("hi")
+
+
+
+    bgVid = document.getElementById("bgVid");
+
+    // bgVid.onload = function () {
+        console.log("hi")
+        bgVid.play();
+    // }
 
     
 
